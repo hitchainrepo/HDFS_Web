8fd1e068935deb53141a4b15a47e30841d7fe208
nigel007 <609441862@qq.com>
2018-11-11 22:51:41 +0800

    change cron_job

--- BackEnd/cron_job.py
+++ BackEnd/cron_job.py
@@ -2,7 +2,7 @@
 
 from BackEnd.models import *
 from BackEnd.utils import *
-import requests
+from urllib.request import Request, urlopen
 
 def cron_check_storage():
     nodeIds = StorageReport.objects.values_list('node_id').order_by('-id')
@@ -14,16 +14,17 @@ def cron_check_storage():
 
     for nodeId in nodeIdList:
         reqUrl = "http://localhost:5001/api/v0/ping?arg=" + nodeId + "&count=1"
-        responseResult = requests.get(reqUrl)
-        responseStatus = responseResult.status_code
+        req = Request(reqUrl)
         currentTime = getCurrentTime()
         storageCheckItem = StorageCheck()
         storageCheckItem.node_id = nodeId
         storageCheckItem.create_time = currentTime
-        if responseStatus == 200:
-            # check success
-            storageCheckItem.ping_result = True
-        else:
-            # check failed
+        try:
+            response = urlopen(req)
+            if response.status == 200:
+                storageCheckItem.ping_result = True
+            else:
+                storageCheckItem.ping_result = False
+        except Exception as e:
             storageCheckItem.ping_result = False
         storageCheckItem.save()
--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+python manage.py crontab remove
 killall -9 uwsgi
 uwsgi --ini uwsgi.ini
 python manage.py crontab add
