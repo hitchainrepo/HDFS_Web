0c87e125f0ad46bee34013933ea5a45b28913aa9
nigel007 <609441862@qq.com>
2018-12-02 11:45:43 +0800

    change update_time

--- BackEnd/views.py
+++ BackEnd/views.py
@@ -451,7 +451,7 @@ def webservice(request):
                             item.node_id = nodeId
                             item.public_key = pubKey
                             item.create_time = currentTime
-                            item.update_time = None
+                            # item.update_time = None
                             item.save()
                         else:
                             print("already has temporary pub key")
