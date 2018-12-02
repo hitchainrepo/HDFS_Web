62ad444b4df454b855ace77ab0d370c27a6259d4
nigel007 <609441862@qq.com>
2018-12-02 13:00:30 +0800

    change log test

--- BackEnd/views.py
+++ BackEnd/views.py
@@ -440,14 +440,12 @@ def webservice(request):
                         return JsonResponse(data=content, status=status.HTTP_200_OK)
                     else:
                         pubKey = data["pubKey"]
-                        print(pubKey)
                         nodeId = data["nodeId"]
-                        print(nodeId)
                         currentTime = getCurrentTime()
+                        print(currentTime)
 
                         # check node Id
                         temporaryPubKeyItem = TemporaryPubKey.objects.filter(node_id=nodeId)
-                        print(temporaryPubKeyItem)
                         print(len(temporaryPubKeyItem))
                         if temporaryPubKeyItem:
                             print("no temporary pub key")
