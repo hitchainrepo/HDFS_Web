6757b4a75b870b9ccac50e333694e186f8671196
nigel007 <609441862@qq.com>
2018-12-03 10:42:38 +0800

    change verify function

--- BackEnd/utils.py
+++ BackEnd/utils.py
@@ -101,35 +101,12 @@ def verify_sign(pub_key, signature, data):
     signer = PKCS1_v1_5.new(rsakey)
     digest = SHA256.new()
     # Assumes the data is base64 encoded to begin with
-    digest.update(b64decode(data))
+    print(data)
+    digest.update(data)
+    print(signature)
     if signer.verify(digest, b64decode(signature)):
         return True
     return False
-
-def str2key(s):
-    # 对字符串解码
-    b_str = base64.b64decode(s)
-
-    if len(b_str) < 162:
-        return False
-
-    hex_str = ''
-
-    # 按位转换成16进制
-    for x in b_str:
-        h = hex(ord(x))[2:]
-        h = h.rjust(2, '0')
-        hex_str += h
-    # 找到模数和指数的开头结束位置
-    m_start = 29 * 2
-    e_start = 159 * 2
-    m_len = 128 * 2
-    e_len = 3 * 2
-    modulus = hex_str[m_start:m_start + m_len]
-    exponent = hex_str[e_start:e_start + e_len]
-
-    return modulus,exponent
-
 
 if __name__ == "__main__":
     import pymysql
